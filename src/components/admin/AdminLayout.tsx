59e11e29956ff554682267affae37cf277757142
Princewill Isonguyo <princewillisonguyo@gmail.com>
2026-09-10 11:39:07 +0100

    Remove unused closing parenthesis in AdminLayout

--- src/components/admin/AdminLayout.tsx
+++ src/components/admin/AdminLayout.tsx
@@ -160,8 +160,6 @@ export function AdminLayout({ children, title, subtitle, actions }: {
             <Home className="h-4 w-4" />
           </Link>
           <NotificationBell />
-
-            )}
           </div>
         </div>
       </header>
